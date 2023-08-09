f5f6855d17398baea47979c279277fdbb55d0f74
lina <marcecaroc@gmail.com>
2023-08-09 17:32:05 -0500

    Feat: create repository Backend Tour of heroes

---
+++ controllers/hero.js
@@ -0,0 +1,66 @@
+const Hero = require('../models/hero')
+
+const controllerHero = {
+    create: async (req,res) =>{
+        try{
+            const first_name = req.body.first_name
+            const faction = req.body.faction
+            const hp = req.body.hp
+            await Hero.create({
+                first_name:first_name, 
+                family_name:faction,
+                hp:hp
+            })
+            res.json({msg:'Created'})
+        } catch(err){
+            return res.status(500).json({msg:err.message})
+        }
+    },
+    getHeroes: async (req,res) =>{
+        try{
+            const heros = await Hero.find({})
+            res.json(heros)
+        }catch(err){
+            return res.status(500).json({msg:err.message})
+        }
+    },
+    getHeroForId: async (req,res) =>{
+        try{
+            const {id} = req.params
+            const hero = await Hero.findById(id)
+            res.json(hero)
+        }catch(err){
+            return res.status(500).json({msg:err.message})
+        }
+    },
+    updateHero: async (req,res) =>{
+        try{
+            const {id} = req.params
+            const first_name = req.body.first_name
+            const faction = req.body.faction
+            const hp = req.body.hp
+            await Author.findByIdAndUpdate(id,{
+                first_name:first_name, 
+                faction:faction,
+                hp:hp
+            })
+            res.json({msg:'Update'})
+        }catch(err){
+            console.error(err)
+            return res.status(500).json({msg:err.message})            
+        }
+    },
+    deleteHero:async (req,res)=>{
+        try {
+            const {id} = req.params
+            await Hero.findByIdAndDelete(id)
+            res.json({msg:'Delete'})     
+        } catch (err) {
+            console.error(err)
+            return res.status(500).json({msg:err.message})
+        }
+    }
+
+}
+
+module.exports = controllerHero
---
+++ models/hero.js
@@ -0,0 +1,24 @@
+const mongoose = require('mongoose')
+
+const Schema = mongoose.Schema
+const HeroSchema = new Schema ({
+    first_name: {
+        type:String,
+        require: true,
+        maxLength:100
+    },
+    faction:{
+        type:String,
+        require: true,
+        maxLength:100
+    },
+    hp:{
+        type:Number,
+        require:true,
+        default:10
+    }
+    // date_of_birth: { type: Date },
+    // date_of_death: { type: Date },
+})
+
+module.exports = mongoose.model('Hero', HeroSchema)
